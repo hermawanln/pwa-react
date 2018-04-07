95df98e30e6898be8533f28769df8747d8929e74
unknown <hermawan.luthfi@gmail.com>
2018-04-07 11:59:50 +0700

    Routing Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,74 @@
 import React, { Component } from 'react';
-
+import {
+	Route,
+	BrowserRouter as Router,
+	NavLink,
+	Redirect,
+	Link
+} from 'react-router-dom';
+import Button from './Button';
 
 class App extends Component {
-  render() {
-    return (
-      <h1>Hello world</h1>
-    );
+  constructor(){
+  	super();
+  	this.state={isi:[]}
   }
+
+  componentDidMount(){
+  	this.setState({isi:['Klik', 'Saya', 'Dong']})
+  }
+
+  render() {
+
+	const subContact = ({match}) => (
+		<h1>{match.params.name}</h1>
+		)
+  	const Home = () => (
+		<div>
+		<h1>Ini Homenya</h1>
+		</div>
+		);
+		const About = () => (
+		<div>
+		<h1>Tentang</h1>
+		</div>
+		)
+		const Contact = () => (
+		<div>
+		<h1>Contact</h1>
+		<ul>
+		<li><Link to="/Contact/Me">Me</Link></li>
+		<li><Link to="/Contact/You">You</Link></li>
+		<li><Link to="/Contact/Love">Love</Link></li>
+		</ul>
+		<Route path="/Contact/:name" 
+		component={subContact}/>
+		</div>
+	)
+  	let i=1;
+  	let button = this.state.isi.map(value => (<Button key={i++} isi = {value}/>));
+    return (
+    <div>
+      <h1>Hello world</h1>
+      <p>Selamat Datang</p>
+      {button}
+      <Router>
+		<div>
+		<ul>
+		<li><NavLink to="/">Home</NavLink></li>
+		<li><NavLink to="/About">About</NavLink></li>
+		<li><NavLink to="/Contact">Contact</NavLink></li>
+		</ul>
+			<Route path="/" component={Home}/>
+			<Route path="/About" component={About}/>
+			<Route path="/Contact" component={Contact}/>
+		</div>
+	</Router>
+    </div>
+    );
+    
+  }
+  
 }
 
 export default App;
---
+++ src/Button.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Button extends Component {
+  render() {
+    return (
+    	<button>{this.props.isi}</button>
+    );
+  }
+}
+
+export default Button;
